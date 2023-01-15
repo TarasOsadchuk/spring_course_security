28ae223cb582b0c0f5d10bebd784b3e34c0b03d9
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-01-15 20:11:18 +0200

    Adds BD for password

--- src/main/java/com/zaurtregulov/spring/security/configuration/MyConfig.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MyConfig.java
@@ -1,11 +1,15 @@
 package com.zaurtregulov.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "com.zaurtregulov.spring.security")
@@ -20,5 +24,19 @@ public class MyConfig {
         internalResourceViewResolver.setSuffix(".jsp");
 
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
+            dataSource.setUser("root");
+            dataSource.setPassword("10215991Qq-");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return dataSource;
     }
 }
--- src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.zaurtregulov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,9 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,18 +27,21 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder
-                        .username("zaur")
-                        .password("zaur")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder
-                        .username("elena")
-                        .password("elena").roles("HR"))
-                .withUser(userBuilder
-                        .username("ivan")
-                        .password("ivan")
-                        .roles("MANAGER", "HR"));
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder
+//                        .username("zaur")
+//                        .password("zaur")
+//                        .roles("EMPLOYEE"))
+//                .withUser(userBuilder
+//                        .username("elena")
+//                        .password("elena").roles("HR"))
+//                .withUser(userBuilder
+//                        .username("ivan")
+//                        .password("ivan")
+//                        .roles("MANAGER", "HR"));
     }
 }
